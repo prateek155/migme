f4c68a0a842398414b8062b9cee5b38db9db28b2
prateek155 <prateekagrawal120@gmail.com>
2026-08-26 00:27:11 +0530

    Update FilteredOrdersScreen.js

--- src/screens/FilteredOrdersScreen.js
+++ src/screens/FilteredOrdersScreen.js
@@ -35,7 +35,7 @@ const startOfDay = (dt) => new Date(dt.getFullYear(), dt.getMonth(), dt.getDate(
 const isToday = (dateVal) => {
   const ms = toMillis(dateVal);
   if (!ms) return false;
-  return startOfDay(new Date(ms)) === startOfDay(new Date('2026-08-25'));
+  return startOfDay(new Date(ms)) === startOfDay(new Date());
 };
 
 const getDateKey = (dateVal) => {
